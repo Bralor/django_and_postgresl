329da2763c705789e03d347b93280f21dce76e1b
Matous Holinka <matous.holinka@nic.cz>
2020-05-23 08:49:37 +0200

    master: the initial commit of django+postgresql project

---
+++ e_auctions/e_aucs_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mholinka/projects/django_and_postgresl/e_auctions/e_aucs/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
